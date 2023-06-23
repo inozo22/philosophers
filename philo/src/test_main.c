6e5142113e99dc7e22cbcf835041897c1deaac6e
nimai <119419194+inozo22@users.noreply.github.com>
2023-06-23 13:00:34 +0200

    .

--- philo/src/test_main.c
+++ philo/src/test_main.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 17:19:36 by nimai             #+#    #+#             */
-/*   Updated: 2023/06/22 13:06:37 by nimai            ###   ########.fr       */
+/*   Updated: 2023/06/23 12:47:20 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 #include <unistd.h>
 
 int	mails = 0;
+
 pthread_mutex_t	mutex;
 
 void*	routine()
 {
-	for (int i = 0; i < 1000000; i++)
+	for (int i = 0; i < 1000; i++)
 	{
 		pthread_mutex_lock(&mutex);
 		mails++;
@@ -31,12 +32,20 @@ void*	routine()
 
 int	main(int ac, char **av)
 {
-	pthread_t		t1;
-	pthread_t		t2;
+	pthread_t		th[4];
+	int				i;
 
+	i = -1;
 	(void)av;
 	pthread_mutex_init(&mutex, NULL);
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	while (++i < 4)
+	{
+		if (!pthread_create(&th[i], NULL, &routine, NULL) != 0)
+			return (perror ("Failed to create thread"), 1);
+		if (!pthread_join(th[i], NULL) != 0)
+			return (perror ("Failed to join thread"), 2);
+	}
+/* 	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 	{
 		return (1);
 	}
@@ -44,14 +53,30 @@ int	main(int ac, char **av)
 	{
 		return (2);
 	}
-	if (pthread_join(t1, NULL) != 0)
+	if (pthread_create(&t3, NULL, &routine, NULL) != 0)
 	{
 		return (3);
 	}
-	if (pthread_join(t2, NULL) != 0)
+	if (pthread_create(&t4, NULL, &routine, NULL) != 0)
 	{
 		return (4);
+	}*/
+/* 	if (pthread_join(t1, NULL) != 0)
+	{
+		return (5);
 	}
+	if (pthread_join(t2, NULL) != 0)
+	{
+		return (6);
+	}
+	if (pthread_join(t3, NULL) != 0)
+	{
+		return (7);
+	}
+	if (pthread_join(t4, NULL) != 0)
+	{
+		return (8);
+	}  */
 	printf("Number of mails: %d\n", mails);
 /* 	if (ac < 4)
 		return (philo_error(1), 0); */
