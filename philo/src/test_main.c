2bd60cd92c276edb3246174eb3b3442b7287c220
nimai <119419194+inozo22@users.noreply.github.com>
2023-06-26 13:25:19 +0200

    .

--- philo/src/test_main.c
+++ philo/src/test_main.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 17:19:36 by nimai             #+#    #+#             */
-/*   Updated: 2023/06/23 12:47:20 by nimai            ###   ########.fr       */
+/*   Updated: 2023/06/26 12:41:42 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ pthread_mutex_t	mutex;
 
 void*	routine()
 {
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 10000000; i++)
 	{
 		pthread_mutex_lock(&mutex);
 		mails++;
@@ -38,45 +38,19 @@ int	main(int ac, char **av)
 	i = -1;
 	(void)av;
 	pthread_mutex_init(&mutex, NULL);
-	while (++i < 4)
+	while (++i < 8)
 	{
-		if (!pthread_create(&th[i], NULL, &routine, NULL) != 0)
+		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
 			return (perror ("Failed to create thread"), 1);
-		if (!pthread_join(th[i], NULL) != 0)
+		printf("Thread %d has started\n", i);
+	}
+	i = -1;
+	while (++i < 8)
+	{
+		if (pthread_join(th[i], NULL) != 0)
 			return (perror ("Failed to join thread"), 2);
+		printf("Thread %d has finished execution\n", i);
 	}
-/* 	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-	{
-		return (1);
-	}
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
-	{
-		return (2);
-	}
-	if (pthread_create(&t3, NULL, &routine, NULL) != 0)
-	{
-		return (3);
-	}
-	if (pthread_create(&t4, NULL, &routine, NULL) != 0)
-	{
-		return (4);
-	}*/
-/* 	if (pthread_join(t1, NULL) != 0)
-	{
-		return (5);
-	}
-	if (pthread_join(t2, NULL) != 0)
-	{
-		return (6);
-	}
-	if (pthread_join(t3, NULL) != 0)
-	{
-		return (7);
-	}
-	if (pthread_join(t4, NULL) != 0)
-	{
-		return (8);
-	}  */
 	printf("Number of mails: %d\n", mails);
 /* 	if (ac < 4)
 		return (philo_error(1), 0); */
